62f7e2f1e241649abfead483af2f049a09c4c97f
nojito42 <nojito42@gmail.com>
2025-06-12 13:53:44 +0200

    added basic infos

--- FollowMe.cs
+++ FollowMe.cs
@@ -5,6 +5,7 @@ using ExileCore.PoEMemory.MemoryObjects;
 using ExileCore.Shared.Helpers;
 using Microsoft.VisualBasic;
 using SharpDX;
+using System;
 using System.Collections.Generic;
 using System.Windows.Forms;
 using Vector2 = System.Numerics.Vector2;
@@ -71,6 +72,38 @@ public class FollowMe : BaseSettingsPlugin<FollowMeSettings>
 
     public List<PartyElementPlayerElement> partyElements = [];
     public PartyElementPlayerInfo partyLeaderInfo = null;
+
+    private DateTime _lastTeleportTime = DateTime.MinValue;
+    private readonly TimeSpan _teleportCooldown = TimeSpan.FromMilliseconds(100); // Cooldown de 5 secondes
+
+    private void TryTeleportToLeader(PartyElementPlayerElement leaderElement)
+    {
+        if (DateTime.Now - _lastTeleportTime < _teleportCooldown)
+        {
+            LogMessage("Teleport action on cooldown.");
+            return;
+        }
+
+        var ui = this.GameController.IngameState.IngameUi;
+
+        if (leaderElement.TeleportButton.IsActive && !GameController.Area.CurrentArea.IsHideout)
+        {
+            LogMessage($"Teleporting to party leader {leaderElement.PlayerName} in {leaderElement.ZoneName}...");
+
+            var centerTP = leaderElement.TeleportButton.GetClientRectCache.Center.ToVector2Num();
+            Input.SetCursorPos(centerTP);
+            Input.Click(MouseButtons.Left);
+
+            if (ui.PopUpWindow != null && ui.PopUpWindow.ChildCount > 0)
+            {
+                Input.KeyPressRelease(Keys.Enter);
+                var centerscreen = GameController.Window.GetWindowRectangle().Center.ToVector2Num();
+                Input.SetCursorPos(centerscreen);
+            }
+
+            _lastTeleportTime = DateTime.Now;
+        }
+    }
     public override bool Initialise()
     {
         return true;
@@ -96,28 +129,10 @@ public class FollowMe : BaseSettingsPlugin<FollowMeSettings>
             {
                 if (partyLeaderInfo.IsInDifferentZone)
                 {
-                    if(leaderElement.TeleportButton.IsActive && !GameController.Area.CurrentArea.IsHideout)
-                    {
-                        LogMessage($"Teleporting to party leader {leaderElement.PlayerName} in {leaderElement.ZoneName}...");
-                        var centerTP = leaderElement.TeleportButton.GetClientRectCache.Center.ToVector2Num();
-                        Input.SetCursorPos(centerTP);
-                        Input.Click(MouseButtons.Left);
-
-                        if(ui.PopUpWindow != null && ui.PopUpWindow.ChildCount > 0)
-                        {
-                            Input.KeyPressRelease(Keys.Enter);
-                            var centerscreen = GameController.Window.GetWindowRectangle().Center.ToVector2Num();
-                            Input.SetCursorPos(centerscreen);
-                        }
-                    }
-                }
-                else
-                {
-                    LogMessage($"Party leader {leaderElement.PlayerName} is in the same zone: {leaderElement.ZoneName}");
+                    TryTeleportToLeader(leaderElement);
                 }
             }
-
-
+        
         }
 
         return null;
