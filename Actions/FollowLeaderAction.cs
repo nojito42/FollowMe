bfd314975d69ebf65f699a53731fe3a65dba2a00
nojito42 <nojito42@gmail.com>
2025-06-12 17:12:12 +0200

    add...

--- Actions/FollowLeaderAction.cs
+++ Actions/FollowLeaderAction.cs
@@ -13,7 +13,7 @@ public class FollowLeaderAction(FollowMe plugin) : IGameAction
     private readonly FollowMe plugin = plugin;
 
     public int Priority => 1; // Priorité plus haute que TP par exemple
-    public TimeSpan Cooldown => TimeSpan.FromMilliseconds(80); // plus rapide que TP
+    public TimeSpan Cooldown => TimeSpan.FromMilliseconds(111); // plus rapide que TP
     public string MutexKey => "followLeader";
 
     public bool CanExecute()
