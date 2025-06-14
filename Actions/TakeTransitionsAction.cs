cc4a3dc8b2b3dbf9799b1ff1fd53fdb90a262ba4
nojito42 <nojito42@gmail.com>
2025-06-14 15:32:22 +0200

    add...

--- Actions/TakeTransitionsAction.cs
+++ Actions/TakeTransitionsAction.cs
@@ -104,7 +104,7 @@ public class TakeTransitionsAction(FollowMe plugin) : IGameAction
 
          
 
-           plugin.LogMessage($"[Follow] Téléportation vers '{cachedTransitionEntity.RenderName}' à l’écran {pl.Label.PositionNum}.", 1, SharpDX.Color.Green);
+           //plugin.LogMessage($"[Follow] Téléportation vers '{cachedTransitionEntity.RenderName}' à l’écran {pl.Label.PositionNum}.", 1, SharpDX.Color.Green);
         // Set cursor position to the center of the label
         var wts = plugin.GameController.IngameState.Camera.WorldToScreen(cachedTransitionEntity.BoundsCenterPosNum);
         if (wts == Vector2.Zero)
